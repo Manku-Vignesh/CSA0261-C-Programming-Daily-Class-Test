ed989f3d1b21b637a2cd70c74cb2befa46ad4fed
RKvignesh07 <119102507+RKvignesh07@users.noreply.github.com>
2023-03-30 14:11:03 +0530

    Add files via upload

---
+++ descending and ascending order.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int i[10]={3,6,9,12,15,18,21,24,27,30};
+
+    // ascending order
+    for (i=3; i<=30; i++3;) 
+	{
+        printf("%d ", i);
+    }
+    printf("\n");
+// descending order
+    for (i=30; i>=3; i--30;) 
+	{
+        printf("%d ", i);
+    }
+    printf("\n");
+
+    return 0;
+}
+
