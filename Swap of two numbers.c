858256a5807ca49f1d9b411001858a4eb867580c
RKvignesh07 <119102507+RKvignesh07@users.noreply.github.com>
2023-03-30 22:19:51 +0530

    Add files via upload

---
+++ Swap of two numbers.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main() {
+  double first, second, temp;
+  printf("Enter first number: ");
+  scanf("%lf", &first);
+  printf("Enter second number: ");
+  scanf("%lf", &second);
+
+  // value of first to temp
+  temp = first;
+
+  // value of second to first
+  first = second;
+
+  // value of temp  to second
+  second = temp;
+
+  // %.2lf displays number up to 2 decimal points
+  printf("\nAfter swapping, first number = %.2lf\n", first);
+  printf("After swapping, second number = %.2lf", second);
+  return 0;
+}
+
